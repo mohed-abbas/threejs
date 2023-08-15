7922ed1d0dff9bad0e3b2af8dd2ab6e7dd8cfff8
Mohed Abbas <73742004+mohed332@users.noreply.github.com>
2023-08-15 23:14:36 +0200

    backdrop corrections

--- client/src/canvas/BackDrop.jsx
+++ client/src/canvas/BackDrop.jsx
@@ -10,7 +10,7 @@ const BackDrop = () => {
       ref={shadows}
       position={[0,0, -0.14]}
       temporal
-      frames={30}
+      frames={60}
       alphaTest={0.85}
       scale={10}
       rotation={[Math.PI / 2 , 0, 0]}
@@ -20,17 +20,17 @@ const BackDrop = () => {
       <RandomizedLight 
       amount={4}
       radius={9}
-      intensity={0.55}
-      ambient={0.25}
+      intensity={1}
+      ambient={0.75}
       position={[5, 5, -10]}
       />
-    {/* SEcond light to have better shadows. */}
+    {/* Second light to have better shadows. */}
     <RandomizedLight 
-      amount={6}
-      radius={5}
-      intensity={0.55}
-      ambient={0.25}
-      position={[-5, 5, 9]}
+      amount={4}
+      radius={7}
+      intensity={0.75}
+      ambient={0.5}
+      position={[-5, 5, -9]}
       />
     </AccumulativeShadows>
   )
